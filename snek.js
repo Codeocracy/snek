e0036cb01c4557bc02ecf4fc7d9475dd926b86df
Lars Borchert <40208002+Lars314@users.noreply.github.com>
2019-10-07 14:07:30 -0700

    Update snek.js

--- snek.js
+++ snek.js
@@ -3,6 +3,15 @@ const client = new Discord.Client();
 const config = require("config.json");
 const pypyjs = require("./lib/pypyjs.js");
 
+client.on("ready", () => {
+    console.log("hiss");
+  });
+
+client.on('disconnect', function(erMsg, code) {
+	console.log('---- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '----');
+	client.connect();
+});
+
 client.on("message", (message) => {
 
     if (message.startsWith("/python")) {
