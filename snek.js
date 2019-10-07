458d4eda4d5316b3cb728abb3fe2ff7a4a7fe46a
Lars Borchert <40208002+Lars314@users.noreply.github.com>
2019-10-07 13:40:05 -0700

    Initial creation

---
+++ snek.js
@@ -0,0 +1,23 @@
+var Discord = require("discord.io");
+var config = require(".config.json");
+var fs = require('fs');
+const pypyjs = require("./lib/pypyjs.js");
+
+var bot = new Discord.Client({
+    token: config.token,
+    autorun: true
+})
+
+pypyjs.read().then(function() {
+
+})
+
+bot.on('message', function (user, userID, channleID, message, evt) {
+
+    if (message.startsWith("/python")) {
+        message.channel.send(pypyjs.exec(message.content));
+        return;
+    } 
+});
+
+client.login(config.token);
