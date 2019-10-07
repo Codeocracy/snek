5a0c7b32156532f105fe377d583b8f22cbf41256
Lars Borchert <40208002+Lars314@users.noreply.github.com>
2019-10-07 14:19:10 -0700

    Update snek.js

--- snek.js
+++ snek.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("config.json");
+const config = require("./config.json");
 const pypyjs = require("./lib/pypyjs.js");
 
 pypyjs.ready().then(function() {
