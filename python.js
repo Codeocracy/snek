b94285ef836e153f2ada50dbe7a214b49030cdaa
Lars <40208002+Lars314@users.noreply.github.com>
2019-10-07 19:47:23 -0700

    Update python.js

--- python.js
+++ python.js
@@ -1,6 +1,8 @@
 const pypyjs = require("./lib/pypyjs.js");
 
-pypyjs.ready().then(function() {});
+pypyjs.ready().then(function() {
+    console.log("hiss");
+});
 
 function runPy(text) {
     return pypyjs.exec(text);
