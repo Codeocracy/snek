2811dcfd69b229353bd5f4b67f089a81d7bdd57c
Lars <40208002+Lars314@users.noreply.github.com>
2019-10-07 19:19:40 -0700

    Update snek.js

--- snek.js
+++ snek.js
@@ -4,17 +4,17 @@ const config = require("./config.json");
 const pypyjs = require("./lib/pypyjs.js");
 
 client.on("ready", () => {
-    console.log("hiss discord");
+    //console.log("hiss discord");
 });
 
 pypyjs.ready().then(function() {
-    console.log("hiss pypy")
+    //console.log("hiss pypy")
 });
 
 client.on("message", (message) => {
 
     if (message.content.indexOf("/python") == 0) {
-        message.channel.send(pypyjs.exec('print("memes")'));
+        message.channel.send(pypyjs.exec(message.content.substring(8)));
         return;
     } 
 });
