214f6d7ad452a31c57e66c3999d93095ddb59aaf
Lars <40208002+Lars314@users.noreply.github.com>
2019-10-07 19:32:25 -0700

    Update snek.js

--- snek.js
+++ snek.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./snekConfig.json");
-//const pypyjs = require("./lib/pypyjs.js");
+const pypyjs = require("./lib/pypyjs.js");
 
 client.on("ready", () => {
     console.log("hiss discord");
